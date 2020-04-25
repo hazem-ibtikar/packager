688f9990cc05ff98a346e1ad9546f98724cf56ed
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-04-25 16:44:36 +0200

    [add] add settings screen

--- app/src/main/java/com/example/mvvmstarterproject/ui/home/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/mvvmstarterproject/ui/home/settings/SettingsFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.mvvmstarterproject.R
 import com.example.mvvmstarterproject.base.BaseFragment
 import kotlinx.android.synthetic.main.app_bar_home.*
@@ -21,10 +19,9 @@ class SettingsFragment : BaseFragment<SettingsViewModel>() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
         viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
         })
         return root
     }
