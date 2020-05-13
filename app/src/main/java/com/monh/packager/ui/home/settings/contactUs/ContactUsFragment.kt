2d63b450868171f2e767e5b4ac689c6589f5ea89
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-05-13 18:53:43 +0200

    [add] start call instead of open diler

--- app/src/main/java/com/monh/packager/ui/home/settings/contactUs/ContactUsFragment.kt
+++ app/src/main/java/com/monh/packager/ui/home/settings/contactUs/ContactUsFragment.kt
@@ -1,13 +1,21 @@
 package com.monh.packager.ui.home.settings.contactUs
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import com.monh.packager.R
 import com.monh.packager.base.BaseFragment
 import kotlinx.android.synthetic.main.app_bar_home.*
@@ -34,24 +42,46 @@ class ContactUsFragment : BaseFragment<ContactUsViewModel>() {
 
     private fun handleContactsClick() {
         phoneIcon.setOnClickListener {
-            val intent = Intent(Intent.ACTION_DIAL).apply {
-                data = Uri.parse("tel:${viewModel.contactUsLiveData.value?.contacts?.supportPhoneno}")
-            }
-            if (intent.resolveActivity(requireActivity().packageManager) != null) {
-                startActivity(intent)
-            }
+            callPermission()
         }
 
         mailIcon.setOnClickListener {
             val intent = Intent(Intent.ACTION_SENDTO).apply {
                 data = Uri.parse("mailto:${viewModel.contactUsLiveData.value?.contacts?.supportEmail}") // only email apps should handle this
             }
+
             if (intent.resolveActivity(requireActivity().packageManager) != null) {
                 startActivity(intent)
             }
         }
     }
 
+    private fun callPermission(){
+        Dexter.withActivity(activity)
+            .withPermission(Manifest.permission.CALL_PHONE)
+            .withListener(object : PermissionListener{
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                    val intent = Intent(Intent.ACTION_CALL).apply {
+                        data = Uri.parse("tel:${viewModel.contactUsLiveData.value?.contacts?.supportPhoneno}")
+                    }
+                    if (intent.resolveActivity(requireActivity().packageManager) != null) {
+                        startActivity(intent)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                    Toast.makeText(requireContext(), getString(R.string.nedd_call_permission), Toast.LENGTH_SHORT).show()
+                }
+
+            }).check()
+    }
     private fun handleContactUsResponse() {
         viewModel.contactUsLiveData.observe(viewLifecycleOwner, Observer {
             it?.let {
