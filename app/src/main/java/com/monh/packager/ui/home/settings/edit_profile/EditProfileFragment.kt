9e1912fe3fef3540965bc9db4c3534b425b38201
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-05-21 01:31:07 +0200

    [add] edit profile select image

--- app/src/main/java/com/monh/packager/ui/home/settings/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/monh/packager/ui/home/settings/edit_profile/EditProfileFragment.kt
@@ -1,14 +1,18 @@
 package com.monh.packager.ui.home.settings.edit_profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 
 import com.monh.packager.R
 import com.monh.packager.base.BaseFragment
+import com.opensooq.supernova.gligar.GligarPicker
 import kotlinx.android.synthetic.main.app_bar_home.*
+import kotlinx.android.synthetic.main.edit_profile_fragment.*
 
 class EditProfileFragment : BaseFragment<EditProfileViewModel>() {
 
@@ -27,6 +31,18 @@ class EditProfileFragment : BaseFragment<EditProfileViewModel>() {
         super.onActivityCreated(savedInstanceState)
         setUpFragmentTitle()
         handleToolBar()
+        handleImageSelection()
+    }
+
+    private fun handleImageSelection() {
+        selectImage.setOnClickListener {
+            GligarPicker()
+                .requestCode(PICKER_REQUEST_CODE)
+                .withFragment(this)
+                .limit(1)
+                .disableCamera(true)
+                .show()
+        }
     }
 
     private fun handleToolBar() {
@@ -39,4 +55,14 @@ class EditProfileFragment : BaseFragment<EditProfileViewModel>() {
     private fun setUpFragmentTitle() {
         activity?.toolbar?.title = context?.getString(R.string.edit_profile)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            PICKER_REQUEST_CODE -> {
+                Glide.with(this).load(data?.extras?.getStringArray(GligarPicker.IMAGES_RESULT)?.get(0)).into(profile_image)
+            }
+        }
+    }
 }
+const val PICKER_REQUEST_CODE = 123
